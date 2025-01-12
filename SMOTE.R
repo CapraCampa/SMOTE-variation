f4bc4d364a2d332f7c06d7ae49cbf6507dae1749
vale <destasiovaleria16@gmail.com>
2025-01-12 11:28:12 +0100

    Upload R script

---
+++ SMOTE.R
@@ -0,0 +1,12 @@
+# simulate data
+# where each dataset has different parameters
+# (size of the dataset and proportion of rare examples over all examples)
+
+
+# balance dataset with SMOTE
+# balance dataset with SMOTE variant
+
+
+#apply models (tree and logistic regression) to datasets
+
+#compute metrics to compare our variant to the original technique
